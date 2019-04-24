9996423d4781a771b13e9fb43460374d23de250a
Donnie Adams <adams.dq.1@gmail.com>
2019-04-23 22:42:25 -0400

    Corrected typo in get_logs cloud function

--- functions/get_logs/main.py
+++ functions/get_logs/main.py
@@ -1,22 +1,3 @@
-#def hello_world(request):
-#    """Responds to any HTTP request.
-#    Args:
-#        request (flask.Request): HTTP request object.
-#    Returns:
-#        The response text or any set of values that can be turned into a
-#        Response object using
-#        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-#    """
-
-#    request_json = request.get_json()
-#    if request.args and 'message' in request.args:
-#        return "args: " + request.args.get('message')
-#    elif request_json and 'message' in request_json:
-#        return "json: " + request_json['message']
-#    else:
-#        return f'Hello World!'
-
-
 from os import getenv
 
 import pymysql
@@ -56,7 +37,7 @@ def __get_cursor():
         return mysql_conn.cursor()
 
 
-def mysql_demo(request):
+def get_logs(request):
     global mysql_conn
 
     # Initialize connections lazily, in case SQL access isn't needed for this
