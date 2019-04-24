499fb81366e04ac0c9d09744bb77afa8cc75b28b
Donnie Adams <adams.dq.1@gmail.com>
2019-04-23 23:22:44 -0400

    Corrected typo in new_log cloud function

--- functions/new_log/main.py
+++ functions/new_log/main.py
@@ -37,7 +37,7 @@ def __get_cursor():
         return mysql_conn.cursor()
 
 
-def get_logs(request):
+def new_log(request):
     global mysql_conn
 
     # Initialize connections lazily, in case SQL access isn't needed for this
