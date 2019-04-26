c96b263f9b7d09d5a1f5c86920a400afafc118da
Nolan <nsharple@purdue.edu>
2019-04-26 09:40:24 -0400

    testtttt

--- functions/get_logs/main.py
+++ functions/get_logs/main.py
@@ -107,9 +107,9 @@ def get_logs(request):
             result += "</span>"
 
             # Delete Button
-            result += "<a class=\"btn btn-dark pull-right bg-danger btn-sm\" role=\"button\" onclick=\"deleteEntry("
+            result += "<a class=\"btn btn-dark pull-right bg-danger btn-sm\" role=\"button\" onclick=\"deleteEntry(\""
             result += str(row.get('log_id'))
-            result += ", \""
+            result += "\", \""
             result += "log"
             result += "\")\"><i class=\"fa fa-times\"></i></a>"
 
