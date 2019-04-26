4052a96fbaaecc36eb2888cdf8c0e8b3529bd906
Nolan <nsharple@purdue.edu>
2019-04-26 04:30:11 -0400

    test54654

--- functions/get_logs/main.py
+++ functions/get_logs/main.py
@@ -109,8 +109,9 @@ def get_logs(request):
             # Delete Button
             result += "<a class=\"btn btn-dark pull-right bg-danger btn-sm\" role=\"button\" onclick=\"deleteEntry("
             result += str(row.get('log_id'))
-            type1 = "log"
-            result += ", " + type1 + ")\"><i class=\"fa fa-times\"></i></a>"
+            result += ", \""
+            result += "log"
+            result += "\")\"><i class=\"fa fa-times\"></i></a>"
 
 			# Rest
             result += "</span></div></div></div></div></div>"
